42836d0a0b9cd9e3bced88b4db27881b56c8e8d3
Taylor Lovett <tlovett88@gmail.com>
2015-02-19 23:40:34 -0500

    Test argv processing

---
+++ test/command.js
@@ -0,0 +1,119 @@
+'use strict';
+
+var proxyquire = require('proxyquire');
+var assert = require('assert');
+
+var mockContainer = function() { };
+/**
+ * Mock the Containers object
+ */
+var Command = proxyquire('../lib/command', { './container': { container: mockContainer } });
+
+describe('command', function() {
+
+	describe('#processArgs()', function() {
+
+		/**
+		 * Simple dockunit call with no arguments
+		 */
+		it('Test no special args', function() {
+			Command.setGlobals();
+
+			var argv = {
+				_: []
+			};
+
+			Command.processArgs(argv);
+
+			assert.equal(global.config.path, process.cwd());
+			assert.equal(global.config.verbose, false);
+			assert.equal(global.config.help, false);
+			assert.equal(Object.keys(global.testArgs).length, 1);
+		});
+
+		/**
+		 * Set a custom path
+		 */
+		it('Test custom path', function() {
+			Command.setGlobals();
+
+			var argv = {
+				_: ['path']
+			};
+
+			Command.processArgs(argv);
+
+			assert.equal(global.config.path, 'path');
+			assert.equal(global.config.verbose, false);
+			assert.equal(global.config.help, false);
+			assert.equal(Object.keys(global.testArgs).length, 1);
+			assert.equal(global.testArgs._.length, 0);
+		});
+
+		/**
+		 * Set a simple command to be verbose
+		 */
+		it('Test verbose command', function() {
+			Command.setGlobals();
+
+			var argv = {
+				_: [],
+				'du-verbose': true
+			};
+
+			Command.processArgs(argv);
+
+			assert.equal(global.config.path, process.cwd());
+			assert.equal(global.config.verbose, true);
+			assert.equal(global.config.help, false);
+			assert.equal(Object.keys(global.testArgs).length, 1);
+			assert.equal(global.testArgs._.length, 0);
+		});
+
+		/**
+		 * Pass just a simple option to the test command
+		 */
+		it('Test pass argument to test command', function() {
+			Command.setGlobals();
+
+			var argv = {
+				_: [],
+				special: 'test'
+			};
+
+			Command.processArgs(argv);
+
+			assert.equal(global.config.path, process.cwd());
+			assert.equal(global.config.verbose, false);
+			assert.equal(global.config.help, false);
+			assert.equal(Object.keys(global.testArgs).length, 2);
+			assert.equal(global.testArgs.special, 'test');
+			assert.equal(global.testArgs._.length, 0);
+		});
+
+		/**
+		 * Pass a path as well as a test arg. Set to verbose and pass two options to the test command.
+		 */
+		it('Test complex command', function() {
+			Command.setGlobals();
+
+			var argv = {
+				_: ['path', 'test-arg'],
+				special: 'test',
+				'du-verbose': true,
+				special2: true
+			};
+
+			Command.processArgs(argv);
+
+			assert.equal(global.config.path, 'path');
+			assert.equal(global.config.verbose, true);
+			assert.equal(global.config.help, false);
+			assert.equal(Object.keys(global.testArgs).length, 3);
+			assert.equal(global.testArgs.special, 'test');
+			assert.equal(global.testArgs.special2, true);
+			assert.equal(global.testArgs._.length, 1);
+		});
+	});
+
+});
